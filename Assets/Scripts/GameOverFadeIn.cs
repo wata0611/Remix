bacd814cd71a65dc9898ca93b431e4b12870a3b7
MutoDaiki <52269946+MutoDaiki@users.noreply.github.com>
2022-12-17 18:01:04 +0900

    Gameoverscene

---
+++ Assets/Scripts/GameOverFadeIn.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameOverFadeIn : MonoBehaviour
+{
+    public float speed = 0.01f;  //透明化の速さ
+    float alfa;    //A値を操作するための変数
+    float red, green, blue;    //RGBを操作するための変数
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Panelの色を取得
+        red = GetComponent<Image>().color.r;
+        green = GetComponent<Image>().color.g;
+        blue = GetComponent<Image>().color.b;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Image>().color = new Color(red, green, blue, alfa);
+        alfa += speed;
+    }
+}
